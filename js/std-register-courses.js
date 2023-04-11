57da0be0424ab06a4c4e9abc05b7eaf42d5a0f63
3llam <adham32003200@gmail.com>
2023-04-11 18:13:59 +0200

    add scriptes to register course page

---
+++ js/std-register-courses.js
@@ -0,0 +1,89 @@
+// Move selected elements to right (Selected coureses)
+var selectedOptions1 = new Array();
+const button1 = document.getElementById("go_right");
+button1.addEventListener("click", function (event) {
+    event.preventDefault();
+    const select1 = document.getElementById("ecourses");
+    const select2 = document.getElementById("rcourses");
+    console.log("before submint: ");
+    console.log(selectedOptions1);
+    var len = selectedOptions1.length;
+    for (var i = 0; i < len; i++) {
+        select2.appendChild(selectedOptions1[0]);
+        selectedOptions1[0].classList.remove("selected");
+        selectedOptions1.splice(0, 1);
+    }
+    console.log("after submint: ");
+    console.log(selectedOptions1);
+});
+
+// Style and add the selected element to selected_Options
+const mySelect1 = document.getElementById("ecourses");
+
+mySelect1.addEventListener("click", (event) => {
+    const clickedOption = event.target;
+    clickedOption.selected = !clickedOption.selected;
+    if (clickedOption.id == 'ecourses') return;
+    clickedOption.classList.toggle("selected");
+    const indexToRemove = selectedOptions1.indexOf(clickedOption);
+    console.log("before click: ");
+    console.log(selectedOptions1);
+    if (indexToRemove !== -1) {
+        selectedOptions1.splice(indexToRemove, 1);
+    }
+    else {
+        selectedOptions1.push(clickedOption);
+    }
+    console.log("after click: ");
+    console.log(selectedOptions1);
+});
+
+// Move the selected options to the left (Eligible courses)
+var selectedOptions2 = new Array();
+const button2 = document.getElementById("go_left");
+button2.addEventListener("click", function (event) {
+    event.preventDefault();
+    const select2 = document.getElementById("ecourses");
+    const select1 = document.getElementById("rcourses");
+    console.log("before submint: ");
+    console.log(selectedOptions2);
+    var len = selectedOptions2.length;
+    for (var i = 0; i < len; i++) {
+        select2.appendChild(selectedOptions2[0]);
+        selectedOptions2[0].classList.remove("selected");
+        selectedOptions2.splice(0, 1);
+    }
+    console.log("after submint: ");
+    console.log(selectedOptions2);
+});
+
+// Style and add the selected element to selected_Options
+const mySelect2 = document.getElementById("rcourses");
+
+mySelect2.addEventListener("click", (event) => {
+    const clickedOption = event.target;
+    clickedOption.selected = !clickedOption.selected;
+    if (clickedOption.id == 'rcourses') return;
+    clickedOption.classList.toggle("selected");
+    const indexToRemove = selectedOptions2.indexOf(clickedOption);
+    console.log("before click: ");
+    console.log(selectedOptions2);
+    if (indexToRemove !== -1) {
+        selectedOptions2.splice(indexToRemove, 1);
+    }
+    else {
+        selectedOptions2.push(clickedOption);
+    }
+    console.log("after click: ");
+    console.log(selectedOptions2);
+});
+
+// Move All options to the left (Eligible courses) - Reset
+const button3 = document.getElementById("reset");
+button3.addEventListener("click", function () {
+    const select2 = document.getElementById("ecourses");
+    const select1 = document.getElementById("rcourses");
+    while (select1.firstChild) {
+        select2.appendChild(select1.firstChild);
+    }
+});
